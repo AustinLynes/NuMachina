ed1c5046a93d428563d6bdd543d1b2990b08d6c4
AustinLynes <austinlynes94@gmail.com>
2020-07-21 11:04:24 -0700

    initialized NPM Package on NPM starting version control

---
+++ dist/components/foreground/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Foreground = () => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "wrapper"
+  });
+};
+
+export default Foreground;
---
+++ src/components/foreground/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Foreground = () => {
+    return (
+        <div className="wrapper">
+            
+        </div>
+    )
+}
+export default Foreground
